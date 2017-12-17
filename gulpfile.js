a3b63173cbc085750593ee5c6736be7ee4030851
biu <hezhou920@gmail.com>
2017-12-17 22:18:33 +0800

    add gulp plugins: autoprofixer、csssnano and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('gulp-cssnano');
 
 gulp.task('sass', () => {
   return gulp.src('./assets/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: [
+        '>1%',
+        'last 4 versions',
+        'Firefox ESR'
+      ],
+      flexbox: 'no-2009',
+    }))
     .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./assets/dist'));
 });
 
