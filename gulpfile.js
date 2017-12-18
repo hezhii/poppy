248b5bd897a1f3ba198747da421efa68c24d2089
biu <hezhou920@gmail.com>
2017-12-18 15:40:50 +0800

    Use gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('gulp-cssnano');
 const concat = require('gulp-concat');
 
+// Need to install LiveReload extension in the browser
+const livereload = require('gulp-livereload');
+
 gulp.task('sass', () => {
   return gulp.src('./assets/scss/*.scss')
     .pipe(sourcemaps.init())
@@ -20,10 +23,12 @@ gulp.task('sass', () => {
     .pipe(cssnano())
     .pipe(concat('poppy.min.css'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./assets/dist'));
+    .pipe(gulp.dest('./assets/dist'))
+    .pipe(livereload());
 });
 
 gulp.task('sass:watch', function () {
+  livereload.listen();
   gulp.watch('./assets/scss/*.scss', ['sass']);
 });
 
